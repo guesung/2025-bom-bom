cad765c7671b4adf25f00af09af68904e2777df5
guesung <gueit214@naver.com>
2025-07-10 17:20:14 +0900

    feat: webpack ts 설정

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,9 +2,21 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/main.js', 
+  entry: './src/main.ts', 
   output: {
     filename: 'bundle.js', 
     path: path.resolve(__dirname, 'dist'), 
   },
+    module: {
+    rules: [
+      {
+        test: /\.ts$/, // .ts 파일들은
+        use: 'ts-loader', // ts-loader를 거쳐 처리돼요.
+        exclude: /node_modules/ // 외부 모듈은 제외해요.
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js'] // 파일을 import할 때 확장자를 생략할 수 있어요. TypeScript와 JavaScript를 혼용하는 프로젝트에서 설정해두면 좋아요.
+  }
 };
